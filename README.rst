74e785dcf0292aa0a8f97cf36a0605249de96844
Richard Marko <rmarko@fedoraproject.org>
2013-05-17 16:53:26 +0200

    Initial

---
+++ README.rst
@@ -0,0 +1,16 @@
+bash-kvstore
+------------
+
+Because.
+
+Usage ::
+
+  source kv.sh
+  kv_set key value
+  kv_get key
+  kv_set key 1337
+  kv_get key
+  kv_set another wat
+  kv_get another
+  kv_del another
+  cat $KVFILE
---
+++ kv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+KVFILE=$( mktemp )
+
+kv_get() {
+    grep $1 $KVFILE | cut -d'~' -f2-
+}
+
+kv_set() {
+    if [ "x$( get $1 )" == "x" ]; then
+        echo "$1~$2" >> $KVFILE
+    else
+        sed -i "s/^$1~.*$/$1~$2/g" $KVFILE
+    fi
+}
+
+kv_del() {
+    sed -i "/^$1~.*$/d" $KVFILE
+}
